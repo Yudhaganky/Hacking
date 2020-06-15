e5d8ecba64b8f405a12076da1233a1389f878aec
Yudhaganky <66919265+Yudhaganky@users.noreply.github.com>
2020-06-15 15:33:09 +0700

    Add files via upload

---
+++ Hacking.py
@@ -0,0 +1,7 @@
+Import os
+
+Nama=input ("User Name = ") 
+Nama=input ("Password   = ")
+Print("The Password you entered is incorrect. Please try again") 
+Os. System("python2 Hacking1.py#00FF5D#7BFF00")
+
