32873c0b0de5ff80f966084ddeba10a3d72808f2
Yudhaganky <66919265+Yudhaganky@users.noreply.github.com>
2020-06-16 19:30:18 +0700

    Delete Hacking.py

--- Hacking.py
+++
@@ -1,7 +0,0 @@
-Import os
-
-Nama=input ("User Name = ") 
-Nama=input ("Password   = ")
-Print("The Password you entered is incorrect. Please try again") 
-Os. System("python2 Hacking1.py#00FF5D#7BFF00")
-
